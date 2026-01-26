093be6f592dc9fc61ffedca7aa1b5d56ad00b063
robertmreedy <65870974+robertmreedy@users.noreply.github.com>
2026-01-26 15:25:23 -0700

    Update atlwage.py

--- atlwage.py
+++ atlwage.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pandas as pd
 import requests
 
-FRED_CSV_URL = "https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23ebf3fb&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1320&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=FRBATLWGT3MMAUMHWGO&scale=left&cosd=1997-03-01&coed=2025-08-01&line_color=%230073e6&link_values=false&line_style=solid&mark_type=none&mw=3&lw=3&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2025-09-18&revision_date=2025-09-18&nd=1997-03-01"
+FRED_CSV_URL = "https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23ebf3fb&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1320&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=FRBATLWGT3MMAUMHWGO&scale=left&cosd=1997-03-01&line_color=%230073e6&link_values=false&line_style=solid&mark_type=none&mw=3&lw=3&ost=-99999&oet=99999&mma=0&fml=a&fq=Monthly&fam=avg&fgst=lin&line_index=1&transformation=lin&nd=1997-03-01"
 DATA_DIR = Path("data")
 RAW_PATH = DATA_DIR / "atlwage_raw.csv"
 PROC_PATH = DATA_DIR / "atlwage_processed.csv"
